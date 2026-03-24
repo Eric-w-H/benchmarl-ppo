80b51936ac99c5b5579d08b615ec0cc77dbbaecf
Eric Hall <ewhall18@gmail.com>
2026-03-24 16:51:16 -0400

    initial commit, unpopulated run scripts

---
+++ README.md
@@ -0,0 +1,12 @@
+# ECE567 Project 7 Benchmarking
+
+### Setup:
+```uv sync```
+If you encounter installation errors with pettingzoo, comment it out of the `pyproject.toml` and try to install it in your venv some other way. Good luck.
+
+### Running Benchmarks
+```bash
+$ python3 pettingzoo_bench.py
+$ python3 vmas_bench.py
+$ python3 render_results.py
+```
---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "Develop Python on Nix with uv";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              # python
+              pkgs.python3
+              pkgs.uv
+
+              # build tools
+              pkgs.swig
+              pkgs.cmake
+              pkgs.zlib
+
+              # typesetting program
+              pkgs.typst
+            ];
+
+            env = lib.optionalAttrs pkgs.stdenv.isLinux {
+              # Python libraries often load native shared objects using dlopen(3).
+              # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
+              LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+            };
+
+            shellHook = ''
+              export UV_CACHE_DIR=/media/oldhdd/.cache/uv
+              unset PYTHONPATH
+              uv sync
+              . .venv/bin/activate
+              cd ./Multi-Agent-ALE
+              python setup.py install
+              cd ..
+              uv pip install pettingzoo[all]
+            '';
+            #   uv run --with jupyter jupyter lab
+          };
+        }
+      );
+    };
+}
